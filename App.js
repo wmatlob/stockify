34234b3e343085dce060586471952e4fc0d3f885
wmatlob <wmatlob@yahoo.com>
2020-04-04 10:25:04 -0700

    Add files via upload
    Search is working for MarketCap. Sort is working for stock Price instead of Trend, because Trend is empty now.

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+//import Form from './Form';
+//import MyTable from './MyTable';
+//import inventory from './inventory'
+import Screener from './Screener';
+
+function App() {
+  return (
+    <div className="App">
+      <Screener />
+    </div>
+  );
+}
+
+export default App;
